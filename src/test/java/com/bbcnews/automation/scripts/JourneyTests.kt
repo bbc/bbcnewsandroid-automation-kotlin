5e032568affd99a3c3077d5a8c10a4ffd0d8de44
Emma Burton <emma.burton@bbc.co.uk>
2019-05-08 12:11:12 +0100

    Put Journey tests in one class

---
+++ src/test/java/com/bbcnews/automation/scripts/JourneyTests.kt
@@ -0,0 +1,42 @@
+package com.bbcnews.automation.scripts
+
+import org.junit.Test
+
+class JourneyTests : BbcTestCase(
+        "bbc.mobile.news.v3.app.TopLevelActivity",
+        "User Journey") {
+
+    // ToDo implement journey tests
+    // (Can we launch appium in states???)
+
+    @Test
+    fun `user can add items to My News`() {
+//     Tap on an index header (e.g. London)
+//     Tap (+) to add to MyNews
+//     Tap pencil (now appeared) to edit MyNews
+//     Swipe to Add Topics
+//     Scroll and add 4 topics
+//     Add a topic from Search
+//     Swipe back to MyTopics and assert they've been added
+//     Press back twice to go back to Top Stories
+//     Swipe or tap to MyNews
+//     Swipe carousel
+//     Tap a topic
+//     Swipe through the indexes
+    }
+
+    @Test
+    fun `user can view video after browsing`() {
+//     top stories
+//     tap into 1st story
+//     scroll half way down
+//     Swipe to 5th
+//     Play video
+//     Press back (chevron or button?) to go back to Top Stories
+//     Scroll down to Business
+//     Tap Business header
+//     Swipe video carousel
+//     Tap 7th Video item
+    }
+
+}
